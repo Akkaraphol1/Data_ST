def1a68795baab2fad5d59f910e384c895a75a4a
Akkaraphol1 <52001135+Akkaraphol1@users.noreply.github.com>
2021-01-07 21:13:25 +0700

    Add files via upload

---
+++ Data_ST/Data_ST/src/Data_ST/Lab2_7_1.java
@@ -0,0 +1,15 @@
+package Data_ST;
+
+public class Lab2_7_1 {
+   
+        public Lab2_7_1() {
+        System.out.println("__________________________________________________________________________________________________ " );
+        System.out.println("|     Id      |            Name           |        Department       |  Class  |  Term  |  Grade  | " );
+        System.out.println("-------------------------------------------------------------------------------------------------- " );
+        System.out.println("|6206021630065|  Akkraphol Pipathworapoom |  Information Technology |   1RA   |    1   |   2.21  | " );
+        System.out.println("|6206021630065|  Akkraphol Pipathworapoom |  Information Technology |   1RA   |    2   |   2.00  | " );
+        System.out.println("|6206021630065|  Akkraphol Pipathworapoom |  Information Technology |   2RA   |    1   |   2.63  | " );
+        System.out.println("__________________________________________________________________________________________________ " );
+
+        }
+}
---
+++ Data_ST/Data_ST/src/Data_ST/Lab2_7_2.java
@@ -0,0 +1,10 @@
+package Data_ST;
+
+public class Lab2_7_2 {
+    public static void main(String[] args) {
+        // create object from class Lab2_7
+        System.out.println( "===============================" );
+        Lab2_7_1 test = new Lab2_7_1();
+        System.out.println( "===============================" );
+        }
+}
